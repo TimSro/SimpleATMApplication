edc4684a7f313cf7f4bf8f01e507deb5fa309732
TimSro <43541241+TimSro@users.noreply.github.com>
2018-10-01 07:13:53 +0200

    New entity added

--- helloweb/src/main/java/de/tz/helloweb/db/model/MyEntity.java
+++ helloweb/src/main/java/de/tz/helloweb/db/model/MyEntity.java
@@ -8,6 +8,7 @@ import javax.persistence.Id;
 
 @Entity
 public class MyEntity {
+	
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
---
+++ helloweb/src/main/java/de/tz/helloweb/db/model/MyUsers.java
@@ -0,0 +1,17 @@
+package de.tz.helloweb.db.model;
+
+import javax.persistence.*;
+
+@Entity
+public class MyUsers {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+}
